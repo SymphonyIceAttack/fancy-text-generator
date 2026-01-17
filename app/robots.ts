3ffa9793ec971ec81e52be5c298d1df9956e33d2
SymphonyIceAttack <1994370030@qq.com>
2026-01-17 18:03:38 +0800

    feat: remove posts disallow

--- app/robots.ts
+++ app/robots.ts
@@ -8,7 +8,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/admin/", "/api/", "/draft/", "/gallery/", "/posts/"],
+        disallow: ["/admin/", "/api/", "/draft/", "/gallery/"],
       },
       // Exception: Allow the specific post page
       {
